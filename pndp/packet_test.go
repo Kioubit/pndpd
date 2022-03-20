3c499eae825956a7567488ea8338f55ea3878fff
Kioubit <kioubit@localhost.localdomain>
2022-03-20 15:20:28 +0200

    More unit tests

--- pndp/packet_test.go
+++ pndp/packet_test.go
@@ -8,15 +8,15 @@ import (
 	"testing"
 )
 
-type checksumTestCase struct {
-	payloadHexString string
-	want             []byte
-}
-
 func TestCalculateChecksum(t *testing.T) {
 	//fd00::251d:bbbb:bbbb:bbbb → ff02::1:ff00:99 ICMPv6 Neighbor Solicitation for fd00::99 from ad:ad:ad:ad:ad:ad
 
-	cases := []checksumTestCase{
+	type testCase struct {
+		payloadHexString string
+		want             []byte
+	}
+
+	cases := []testCase{
 		{"87 00 1D 12 00 00 00 00 FD 00 00 00 00 00 00 00 00 00 00 00 00 00 00 99 01 01 AD AD AD AD AD AD", //32 (Even)
 			[]byte{0x1D, 0x12}},
 		{"87 00 1D 12 00 00 00 00 FD 00 00 00 00 00 00 00 00 00 00 00 00 00 00 99 01 01 AD AD AD AD AD", //31 (Not even)
@@ -49,4 +49,54 @@ func TestCalculateChecksum(t *testing.T) {
 			t.Errorf("Expected '%x', but got '%x'", tc.want, bChecksum)
 		}
 	}
+}
+
+func TestCheckPacketChecksum(t *testing.T) {
+	type testCase struct {
+		payloadHexString string
+		want             bool
+	}
+
+	cases := []testCase{
+		{"87 00 1D 12 00 00 00 00 FD 00 00 00 00 00 00 00 00 00 00 00 00 00 00 99 01 01 AD AD AD AD AD AD", //32 (Even)
+			true},
+		{"87 00 1D 12 00 00 00 00 FD 00 00 00 00 00 00 00 00 00 00 00 00 00 00 99 01 01 AD AD AD AD AD", //31 (Not even)
+			false},
+	}
+
+	testSrcIP := []byte{0xFD, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x25, 0x1D, 0xBB, 0xBB, 0xBB, 0xBB, 0xBB, 0xBB}
+	testDstIP := []byte{0xFF, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xFF, 0x00, 0x00, 0x99}
+	testHeader, _ := newIpv6Header(testSrcIP, testDstIP)
+
+	for _, tc := range cases {
+		payloadBytes, err := hex.DecodeString(strings.Join(strings.Fields(tc.payloadHexString), ""))
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		got := checkPacketChecksum(testHeader, []byte(payloadBytes))
+		if tc.want != got {
+			t.Errorf("Excpected valid: '%t', but got valid: '%t' with payload '%x'", tc.want, got, payloadBytes)
+		}
+
+	}
+}
+
+func TestIsIpv6(t *testing.T) {
+	type testCase struct {
+		ip   string
+		want bool
+	}
+	cases := []testCase{
+		{"0.0.0.0", false},
+		{"fd", false},
+		{"fd::", true},
+		{"fd00::", true},
+	}
+
+	for _, tc := range cases {
+		if isIpv6(tc.ip) != tc.want {
+			t.Errorf("Expected '%t', but got '%t' for %s", tc.want, !tc.want, tc.ip)
+		}
+	}
+
 }
